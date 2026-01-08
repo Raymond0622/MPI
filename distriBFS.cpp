81990095f3aed8c2bd57b1509b1b0401b0ecaa06
Raymond0622 <raymondpark01@gmail.com>
2026-01-07 20:20:22 -0600

    working variant msg distriBFS

--- distriBFS.cpp
+++ distriBFS.cpp
@@ -5,23 +5,76 @@
 #include <chrono>
 #include <random>
 #include <unordered_set>
+#include <type_traits>
+#include <tuple>
 
 template <typename T, size_t N>
 class Message {
     T msg[N];
-    size_t len = N;
-    public:
-        size_t size() {
-            return len;
-        }
 };
 
-int getQueueSize(std::vector<std::vector<int>>& recbuf) {
+template <typename T, size_t N>
+struct Data {
+    int source;
+    Message<T, N> msg;
+    Data() {};
+    Data(int s) : source(s) {};
+};
+
+using DataType = std::variant<Data<int, 100>, Data<int, 200>, 
+    Data<double, 20>, Data<long long, 50>>;
+
+constexpr size_t numType = std::variant_size_v<DataType>;
+
+int getQueueSize(std::vector<std::vector<int>>& recvcount) {
     int tot = 0;
-    for (int i = 0; i < recbuf.size();i++) {
-        tot += recbuf[i].size();
+    for (int i = 0; i < numType;i++) {
+        for (int j = 0; j < recvcount.size();j++) {
+            tot += recvcount[i][j];
+        }
     }
     return tot;
+};
+
+template <typename T, typename... Ts>
+struct typelist {};
+template <std::size_t N, typename T>
+struct get;
+
+template <std::size_t N, typename T, typename... Ts>
+struct get<N, typelist<T, Ts...>> {
+    using type = typename get<N - 1, typelist<Ts...>>::type;
+};
+template <typename T, typename... Ts>
+struct get<0, typelist<T, Ts...>> {
+    using type = T;
+};
+
+template <typename T>
+void create_mpi_struct(std::size_t idx, T tmp, std::vector<MPI_Datatype>& datatype,
+    MPI_Datatype types[4][2], int lengths[4][2]) {
+        MPI_Aint base;
+        MPI_Aint offset[2];
+        MPI_Datatype MPI_Data;
+        MPI_Get_address(&tmp, &base);
+        MPI_Get_address(&tmp.source, &offset[0]);
+        MPI_Get_address(&tmp.msg, &offset[1]);
+        offset[0] -= base; offset[1] -= base;
+        MPI_Type_create_struct(2, lengths[idx], offset, types[idx], &MPI_Data);
+        MPI_Type_commit(&MPI_Data);
+        datatype.push_back(MPI_Data);
+}
+
+template <std::size_t... Idx>
+void create_my_mpi_types(std::vector<MPI_Datatype>& datatype, 
+    MPI_Datatype types[4][2], 
+    int lengths[4][2], std::integer_sequence<std::size_t, Idx...>) {
+    
+    // very hacky way of getting each alternative type.
+    std::tuple<std::variant_alternative_t<Idx, DataType>...> tup(
+        std::variant_alternative_t<Idx, DataType>{}...);
+        
+    (create_mpi_struct(Idx, std::get<Idx>(tup), datatype, types, lengths), ...);
 }
 
 struct Union {
@@ -77,8 +130,117 @@ struct Graph {
     size_t numP;
 };
 
+int findIdxType(DataType& type) {
+    return type.index();
+}
+
+void bfs(Graph& graph, int& rank, int& size, std::vector<MPI_Datatype>& datatype) {
+    // now since we have different types, we need sendcount, recvount for each type
+    std::vector<std::vector<int>> sendcount, recvcount;
+
+    // senddata[i][j] represents vector jth variant data to send to ith processor
+    std::vector<std::vector<std::vector<DataType>>> senddata;
+    // // find out how many verticies (based on rank) are going to the next BFS level
+    // // This is sort of like degree of a veritices per level
+
+    // // allocate sizes where recbuf[i][j] represent the buffer coming
+    // // from the jth rank processor of type ith variant.
+    std::vector<std::vector<std::vector<DataType>>> recbuf(numType, std::vector<std::vector<DataType>>(size));
+
+    recbuf[2][rank].emplace_back(Data<double, 20>(rank));
+    int depth = 0;
+   
+    int global_flag = 0;
+    while (true) {
+
+        int local_flag = 0;
+        sendcount.assign(numType, std::vector<int>(size, 0));
+        recvcount.assign(numType, std::vector<int>(size, 0));
+        senddata.clear();
+        senddata.assign(size, std::vector<std::vector<DataType>>(numType));
+                
+        for (int i = 0; i < numType;i++) {
+            for (int j = 0; j < size;j++) {
+                // data of type ith variant from jth processor
+                for (auto& msg : recbuf[i][j]) {
+                    // need to get index before visiting
+                    int idx_type = findIdxType(msg);
+                    std::visit([&](auto data) {
+                            int u = data.source;
+                            for (auto v : graph.adjList[u]) {
+                                int own = graph.getOwner(v);
+                                //printf("Rank %d has %d\n", rank, v);
+                                sendcount[i][own]++;
+                                auto new_data = data;
+                                new_data.source = v;
+                                senddata[i][own].push_back(std::move(new_data));
+                            }
+                    }, msg);
+                }   
+            }
+        }
+
+        // // Send the degree to each process
+        // since we need to send count data fro each type, we do for each type of variant
+        for (int i = 0; i < numType;i++) {
+            MPI_Alltoall(sendcount[i].data(), 1, MPI_INT,
+                recvcount[i].data(), 1, MPI_INT, MPI_COMM_WORLD);
+        }
+        recbuf.clear();
+        recbuf.assign(numType, std::vector<std::vector<DataType>>(size));
+        for (int i = 0; i < numType;i++) {
+            for (int j = 0; j < size;j++) {
+                recbuf[i][j].resize(recvcount[i][j]);
+            }
+        }
+        // // Post non-blocking receives
+        std::vector<MPI_Request> requests;
+        for (int i = 0; i < numType;i++) {
+            for (int j = 0; j < size;j++) {
+                MPI_Request req;
+                if (recvcount[i][j] > 0) {
+                    MPI_Irecv(recbuf[i][j].data(), recvcount[i][j],
+                        datatype[i], j, i, MPI_COMM_WORLD, &req);
+                        requests.push_back(req);
+                }  
+            }
+        }
+        for (int i = 0; i < numType;i++) {
+            for (int j = 0; j < size;j++) {
+                if (sendcount[i][j] > 0) {
+                    MPI_Request req;
+                    MPI_Isend(senddata[i][j].data(), sendcount[i][j], 
+                        datatype[i], j, i, MPI_COMM_WORLD, &req);
+                        requests.push_back(req);
+                }
+            }
+        }
+        MPI_Waitall(requests.size(), requests.data(), MPI_STATUSES_IGNORE);
+        depth++;
+        for (int i = 0; i < numType;i++) {
+            for (int j = 0; j < size;j++) {
+                for (auto& v : recbuf[i][j]) {
+                    std::visit([&](auto& msg) {
+                        printf("Rank %d gets vertex %d of msg type %d with depth %d\n", rank, msg.source, i, depth);
+                    }, v);
+                }
+            }
+        };
+        //printf("rank %d %d\n", rank, getQueueSize(recbuf));
+        if (getQueueSize(recvcount) == 0) {
+            local_flag = 1;
+        }
+        MPI_Allreduce(&local_flag, &global_flag, 1, MPI_INT, 
+                MPI_LAND, MPI_COMM_WORLD);
+        if (global_flag) {
+            break;
+        }   
+        //return;
+    }
+}
+
 int main(int argc, char** argv) {
-    int n = 100;
+    int n = 10;
 
     MPI_Init(&argc, &argv);
     int rank, size;
@@ -135,78 +297,19 @@ int main(int argc, char** argv) {
             graph.adjList[i].insert(buffer.begin(), buffer.end());
         }
     }
-    // Message<char, 100> msg;
-    std::vector<int> sendcount, recvcount;
-    std::vector<std::vector<int>> sendvertices;
-    // // find out how many verticies (based on rank) are going to the next BFS level
-    // // This is sort of like degree of a veritices per level
+    // prepare MPI stuff for my own data structs
+    MPI_Datatype types[4][2] = {{MPI_INT, MPI_INT}, {MPI_INT, MPI_INT},
+            {MPI_INT, MPI_DOUBLE}, {MPI_INT, MPI_LONG_LONG}};
+    int lengths[4][2] = {{1, 100}, {1, 200}, {1, 20}, {1, 50}};
+    std::vector<MPI_Datatype> mydatatype;
 
-    // // allocate sizes where recbuf[i] represent the buffer coming
-    // // from the ith rank processor.
-    std::vector<std::vector<int>> recbuf(size);
-    recbuf[rank].push_back(rank);
-    int depth = 0;
-   
-    int global_flag = 0;
-    while (true) {
-        int local_flag = 0;
-        sendcount.assign(size, 0);
-        recvcount.assign(size, 0);
-        sendvertices.clear();
-        sendvertices.resize(size);
-
-        for (int i = 0; i < size;i++) {
-            for (auto& u : recbuf[i]) {
-                for (auto v : graph.adjList[u]) {
-                    int own = graph.getOwner(v);
-                    //printf("Rank %d has %d\n", rank, v);
-                    sendcount[own]++;
-                    sendvertices[own].push_back(v);
-                }
-            }
-        }
-        // // Send the degree to each process
-        MPI_Alltoall(sendcount.data(), 1, MPI_INT, 
-            recvcount.data(), 1, MPI_INT, MPI_COMM_WORLD);
-        
-        recbuf.clear();
-        recbuf.resize(size);
-        for (int i = 0; i < size;i++) {
-            recbuf[i].resize(recvcount[i]);
-        }
-        // // Post non-blocking receives
-        std::vector<MPI_Request> requests;
-        for (int i = 0; i < size;i++) {
-            MPI_Request req;
-            if (recvcount[i] > 0) {
-                MPI_Irecv(recbuf[i].data(), recvcount[i],
-                    MPI_INT, i, 0, MPI_COMM_WORLD, &req);
-                    requests.push_back(req);
-            }  
-        }
-        for (int i = 0; i < size;i++) {
-            if (sendcount[i] > 0) {
-                MPI_Request req;
-                MPI_Isend(sendvertices[i].data(), sendcount[i], 
-                    MPI_INT, i, 0, MPI_COMM_WORLD, &req);
-                    requests.push_back(req);
-            }
-        }
-        MPI_Waitall(requests.size(), requests.data(), MPI_STATUSES_IGNORE);
-        depth++;
-        for (int i = 0; i < size;i++) {
-            for (auto v : recbuf[i])
-                printf("Rank %d gets vertex %d with depth %d\n", rank, v, depth);
-        };
-        if (getQueueSize(recbuf) ==0) {
-            local_flag = 1;
-        }
-        MPI_Allreduce(&local_flag, &global_flag, 1, MPI_INT, 
-                MPI_LAND, MPI_COMM_WORLD);
+    create_my_mpi_types(mydatatype, types, lengths, 
+       std::make_integer_sequence<std::size_t, numType>{});
     
-        if (global_flag) {
-            break;
-        }   
+    bfs(graph, rank, size, mydatatype);
+    for (int i = 0; i < numType;i++) {
+        MPI_Type_free(&mydatatype[i]);
     }
+    
     MPI_Finalize();
 }
